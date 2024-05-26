17fbfc9a5a1fec93b7a927ede4476a58d986cae8
dieloren <tdiegoldc@gmail.com>
2024-05-26 20:56:25 +0200

    feat(FuckingFrontend): Defined website home page

---
+++ resources/js/Components/app/GroupListItems.vue
@@ -0,0 +1,83 @@
+<script setup>
+import GroupItem from "@/Components/app/GroupItem.vue";
+import TextInput from "@/Components/TextInput.vue";
+import {ref} from "vue";
+
+const searchKeyword = ref('');
+
+</script>
+
+<template>
+    <TextInput :model-value="searchKeyword" placeholder="Type to search" class="w-full mt-4" />
+    <div class="mt-3 h-[200px] lg:flex-1 overflow-auto">
+        <div v-if="false" class="text-gray-400 text-center p-3">
+            You are not joined to any groups
+        </div>
+        <div v-else>
+            <GroupItem
+                image="https://picsum.photos/101"
+                title="Group 1"
+                description="This is a a way longer description for this particular group, so that means this description is going to be a lot longer than the other ones."
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Group 1"
+                description="This is a group description."
+            />
+            <GroupItem
+                image="https://picsum.photos/101"
+                title="Group 1"
+                description="This is a a way longer description for this particular group, so that means this description is going to be a lot longer than the other ones."
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Group 1"
+                description="This is a group description."
+            />
+            <GroupItem
+                image="https://picsum.photos/101"
+                title="Group 1"
+                description="This is a a way longer description for this particular group, so that means this description is going to be a lot longer than the other ones."
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Group 1"
+                description="This is a group description."
+            />
+            <GroupItem
+                image="https://picsum.photos/101"
+                title="Group 1"
+                description="This is a a way longer description for this particular group, so that means this description is going to be a lot longer than the other ones."
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Group 1"
+                description="This is a group description."
+            />
+            <GroupItem
+                image="https://picsum.photos/101"
+                title="Group 1"
+                description="This is a a way longer description for this particular group, so that means this description is going to be a lot longer than the other ones."
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Group 1"
+                description="This is a group description."
+            />
+            <GroupItem
+                image="https://picsum.photos/101"
+                title="Group 1"
+                description="This is a a way longer description for this particular group, so that means this description is going to be a lot longer than the other ones."
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Group 1"
+                description="This is a group description."
+            />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/app/PostList.vue
@@ -0,0 +1,61 @@
+<script setup>
+import PostItem from "@/Components/app/PostItem.vue";
+const post1 = {
+    user: {
+        id: 1,
+        avatar: 'https://randomuser.me/api/portraits/men/82.jpg',
+        name: 'Sergio',
+    },
+    group: null,
+    attachments: [
+        {
+            id: 1,
+            name: 'test.png',
+            url: 'https://picsum.photos/1000',
+            mime: 'image/png',
+        },
+        {
+            id: 2,
+            name: 'test2.png',
+            url: 'https://picsum.photos/1000',
+            mime: 'image/png',
+        },
+        {
+            id: 4,
+            name: 'myDoc.docx',
+            url: '#',
+            mime: 'application/msword',
+        }
+    ],
+    body: `
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas tellus a sem viverra, in pulvinar ante lacinia. Aliquam risus ipsum, finibus a leo vel, gravida interdum leo. Nunc rutrum ut est vitae mollis. Aliquam erat volutpat. Donec turpis odio, vulputate eget sollicitudin vel, condimentum nec justo. Maecenas interdum diam a arcu molestie mattis. Nam cursus condimentum ante ut fringilla.</p>
+    `,
+    createdAt: '2021-09-01 15:00',
+}
+const post2 = {
+    user: {
+        id: 2,
+        avatar: 'https://randomuser.me/api/portraits/men/81.jpg',
+        name: 'Diego',
+    },
+    group: {
+        id: 1,
+        name: 'Grupo de prueba',
+    },
+    body: `
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas tellus a sem viverra, in pulvinar ante lacinia. Aliquam risus ipsum, finibus a leo vel, gravida interdum leo. Nunc rutrum ut est vitae mollis. Aliquam erat volutpat. Donec turpis odio, vulputate eget sollicitudin vel, condimentum nec justo. Maecenas interdum diam a arcu molestie mattis. Nam cursus condimentum ante ut fringilla.</p>
+    `,
+    createdAt: '2021-09-01 13:00',
+}
+</script>
+
+<template>
+    <div class="overflow-auto">
+        <PostItem :post="post1"/>
+        <PostItem :post="post2"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
