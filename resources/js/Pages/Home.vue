197767873da987f90e9d797979978921c7c7ebad
dieloren <tdiegoldc@gmail.com>
2024-05-26 15:33:24 +0200

    refactor(Home.vue): changed component name

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+
+});
+
+</script>
+
+<template>
+    <Head title="Social Media Website" />
+    test
+</template>
+
+<style>
+
+</style>
