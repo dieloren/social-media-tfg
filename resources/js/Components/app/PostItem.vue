0c43fb5785521c0f0a6cf1d21103bf60111b7b21
dieloren <tdiegoldc@gmail.com>
2024-06-01 18:58:16 +0200

    Implemented attachments fullscreen display

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -1,16 +1,18 @@
 <script setup>
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
-import { PencilIcon, TrashIcon, EllipsisVerticalIcon, HandThumbUpIcon, ChatBubbleBottomCenterTextIcon, ArrowDownTrayIcon } from '@heroicons/vue/20/solid'
+import {Menu, MenuButton, MenuItems, MenuItem} from '@headlessui/vue'
+import {PencilIcon, TrashIcon, EllipsisVerticalIcon} from '@heroicons/vue/20/solid'
+import {ChatBubbleLeftRightIcon, HandThumbUpIcon, ArrowDownTrayIcon} from '@heroicons/vue/24/outline'
+import {Disclosure, DisclosureButton, DisclosurePanel} from '@headlessui/vue'
 import PostUserHeader from "@/Components/app/PostUserHeader.vue";
-import {router} from "@inertiajs/vue3";
-import { isImage } from '@/helpers.js'
+import {router} from '@inertiajs/vue3'
+import {isImage} from '@/helpers.js'
+import {PaperClipIcon} from "@heroicons/vue/24/solid/index.js";
 
 const props = defineProps({
-    post: Object,
+    post: Object
 })
 
-const emit = defineEmits(['editClick'])
+const emit = defineEmits(['editClick', 'attachmentClick'])
 
 function openEditModal() {
     emit('editClick', props.post)
@@ -23,15 +25,23 @@ function deletePost() {
         })
     }
 }
+
+function openAttachment(ind){
+    emit('attachmentClick', props.post, ind)
+}
+
 </script>
 
 <template>
     <div class="bg-white border rounded p-4 mb-3">
         <div class="flex items-center justify-between mb-3">
-            <PostUserHeader :post="post" :show-time="true" />
-            <Menu as="div" class="relative inline-block text-left z-10">
+            <PostUserHeader :post="post"/>
+            <Menu as="div" class="relative z-10 inline-block text-left">
                 <div>
-                    <MenuButton class="w-8 h-8 rounded-full hover:bg-black/5 transition flex items-center justify-center">
+                    <MenuButton
+                        class="w-8 h-8 rounded-full hover:bg-black/5 transition flex items-center justify-center"
+                    >
+
                         <EllipsisVerticalIcon
                             class="w-5 h-5"
                             aria-hidden="true"
@@ -55,9 +65,9 @@ function deletePost() {
                                 <button
                                     @click="openEditModal"
                                     :class="[
-                                          active ? 'bg-indigo-500 text-white' : 'text-gray-900',
-                                          'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                                        ]"
+                  active ? 'bg-indigo-500 text-white' : 'text-gray-900',
+                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                ]"
                                 >
                                     <PencilIcon
                                         class="mr-2 h-5 w-5"
@@ -70,9 +80,9 @@ function deletePost() {
                                 <button
                                     @click="deletePost"
                                     :class="[
-                                          active ? 'bg-indigo-500 text-white' : 'text-gray-900',
-                                          'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                                        ]"
+                  active ? 'bg-indigo-500 text-white' : 'text-gray-900',
+                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                ]"
                                 >
                                     <TrashIcon
                                         class="mr-2 h-5 w-5"
@@ -88,51 +98,59 @@ function deletePost() {
         </div>
         <div class="mb-3">
             <Disclosure v-slot="{ open }">
-                <div class="ck-content-output" v-if="!open" v-html="post.body.substring(0, 200)" />
-                <DisclosurePanel>
-                    <div class="ck-content-output" v-html="post.body" />
-                </DisclosurePanel>
-                <div v-if="post.body.length > 200" class="flex justify-end">
-                    <DisclosureButton class="text-blue-500 hover:underline">
-                        {{ open ? 'Read Less' : 'Read More'}}
-                    </DisclosureButton>
-                </div>
+                <div class="ck-content-output" v-if="!open" v-html="post.body.substring(0, 200)"/>
+                <template v-if="post.body.length > 200">
+                    <DisclosurePanel>
+                        <div class="ck-content-output" v-html="post.body"/>
+                    </DisclosurePanel>
+                    <div class="flex justify-end">
+                        <DisclosureButton class="text-blue-500 hover:underline">
+                            {{ open ? 'Read less' : 'Read More' }}
+                        </DisclosureButton>
+                    </div>
+                </template>
             </Disclosure>
         </div>
         <div class="grid gap-3 mb-3" :class="[
-            post.attachments.length == 1 ? 'grid-cols-1' : 'grid-cols-2',
+            post.attachments.length === 1 ? 'grid-cols-1' : 'grid-cols-2'
         ]">
             <template v-for="(attachment, ind) of post.attachments.slice(0, 4)">
-                <div class="group aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 relative">
-                    <div v-if="ind === 3 && post.attachments.length > 4" class="absolute left-0 right-0 top-0 bottom-0 z-10 bg-black/60 text-white flex items-center justify-center text-2xl">
+
+                <div @click="openAttachment(ind)"
+                     class="group aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 relative cursor-pointer">
+
+                    <div v-if="ind === 3 && post.attachments.length > 4"
+                         class="absolute left-0 top-0 right-0 bottom-0 z-10 bg-black/60 text-white flex items-center justify-center text-2xl">
                         +{{ post.attachments.length - 4 }} more
                     </div>
-                    <a  :href="route('post.download', attachment)"
-                        class="z-20 opacity-0 group-hover:opacity-100 transition-all w-8 h-8 flex items-center justify-center text-gray-100 bg-gray-700 rounded absolute right-2 top-2 cursor-pointer hover:bg-gray-800">
-                        <ArrowDownTrayIcon class="w-5 h-5"/>
+
+                    <!-- Download-->
+                    <a :href="route('post.download', attachment)"
+                       class="z-20 opacity-0 group-hover:opacity-100 transition-all w-8 h-8 flex items-center justify-center text-gray-100 bg-gray-700 rounded absolute right-2 top-2 cursor-pointer hover:bg-gray-800">
+                        <ArrowDownTrayIcon class="w-4 h-4" />
                     </a>
+                    <!--/ Download-->
 
                     <img v-if="isImage(attachment)"
                          :src="attachment.url"
-                         class="object-cover aspect-square">
+                         class="object-contain aspect-square"/>
+                    <div v-else class="flex flex-col justify-center items-center">
+                        <PaperClipIcon class="w-10 h-10 mb-3"/>
 
-                    <template v-else>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 text-gray-500">
-                            <path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
-                            <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
-                        </svg>
                         <small>{{ attachment.name }}</small>
-                    </template>
+                    </div>
                 </div>
             </template>
         </div>
         <div class="flex gap-2">
-            <button class="text-gray-800 flex gap-1 items-center justify-center bg-gray-100 rounded-lg hover:bg-gray-200 py-2 px-4 flex-1">
+            <button
+                class="text-gray-800 flex gap-1 items-center justify-center bg-gray-100 rounded-lg hover:bg-gray-200 py-2 px-4 flex-1">
                 <HandThumbUpIcon class="w-5 h-5 mr-2"/>
                 Like
             </button>
-            <button class="text-gray-800 flex gap-1 items-center justify-center bg-gray-100 rounded-lg hover:bg-gray-200 py-2 px-4 flex-1">
-                <ChatBubbleBottomCenterTextIcon class="w-5 h-5 mr-2" />
+            <button
+                class="text-gray-800 flex gap-1 items-center justify-center bg-gray-100 rounded-lg hover:bg-gray-200 py-2 px-4 flex-1">
+                <ChatBubbleLeftRightIcon class="w-5 h-5 mr-2"/>
                 Comment
             </button>
         </div>
