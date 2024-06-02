26f5c04dfe9dc2171cdb3585d1c9ccf05824d8ae
dieloren <tdiegoldc@gmail.com>
2024-06-02 18:13:02 +0200

    File size restrictions implemented

--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -28,7 +28,7 @@ const props = defineProps({
 const attachmentExtensions = usePage().props.attachmentExtensions
 const attachmentFiles = ref([])
 const attachmentErrors = ref([])
-const showExtensionsText = ref(false)
+const formErrors = ref({});
 
 const form = useForm({
     body: '',
@@ -46,6 +46,18 @@ const computedAttachments = computed(() => {
     return [...attachmentFiles.value, ...(props.post.attachments || [])]
 })
 
+const showExtensionsText = computed(() => {
+    for (let myFile of attachmentFiles.value) {
+        const file = myFile.file
+        let parts = file.name.split('.')
+        let ext = parts.pop().toLowerCase()
+        if (!attachmentExtensions.includes(ext)) {
+            return true
+        }
+    }
+    return false;
+})
+
 const emit = defineEmits(['update:modelValue', 'hide'])
 
 watch(() => props.post, () => {
@@ -61,7 +73,7 @@ function closeModal() {
 function resetModal() {
     form.reset()
     attachmentFiles.value = []
-    showExtensionsText.value = false
+    formErrors.value = {}
     attachmentErrors.value = []
     if (props.post.attachments) {
         props.post.attachments.forEach(file => file.deleted = false)
@@ -96,6 +108,7 @@ function submit() {
 }
 
 function processErrors(errors) {
+    formErrors.value = errors
     for (const key in errors) {
         if (key.includes('.')) {
             const [, index] = key.split('.')
@@ -105,13 +118,7 @@ function processErrors(errors) {
 }
 
  async function onAttachmentChoose($event) {
-    showExtensionsText.value = false
     for (const file of $event.target.files) {
-        let parts = file.name.split('.')
-        let ext = parts.pop().toLocaleLowerCase()
-        if (!attachmentExtensions.includes(ext)) {
-            showExtensionsText.value = true
-        }
         const myFile = {
             file,
             url: await readFile(file)
@@ -201,7 +208,9 @@ function undoDelete(myFile) {
                                         Files Must be one of the following extensions: <br>
                                         <small>{{attachmentExtensions.join(', ')}}</small>
                                     </div>
-
+                                    <div v-if="formErrors.attachments" class="border-l-4 border-red-500 py-2 px-3 bg-red-100 mt-3 text-gray-800">
+                                        {{formErrors.attachments}}
+                                    </div>
                                     <div class="grid gap-3 my-3" :class="[
                                         computedAttachments.length === 1 ? 'grid-cols-1' : 'grid-cols-2',
                                     ]">
