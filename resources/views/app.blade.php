0beae70e964c3d23e28db99e43d491ae727c0e86
dieloren <tdiegoldc@gmail.com>
2024-06-16 21:22:03 +0200

    Added favicon.ico

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title>Social media TFG</title>
+
+        <!-- Favicon -->
+        <link rel="icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
